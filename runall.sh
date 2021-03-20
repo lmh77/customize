cc4e66ad2144784eefb4288def2d9e52653f7e29
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-20 17:59:25 +0800

    Create runall.sh

---
+++ runall.sh
@@ -0,0 +1,17 @@
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+[ ${JD_DIR} ] && HelpJd=jd || HelpJd=jd.sh
+ScriptsDir=${ShellDir}/scripts
+
+
+diytask=($(cd ${ScriptsDir}; ls *.js | grep -E "cui521_|i-chenzhe_"))
+for ((i=0; i<${#diytask[*]}; i++));
+    do
+      bash jd ${diytask[i]} now
+done
+
+
+lxk0301=($(cd ${ScriptsDir}; ls *.js | grep -v "_jd_" |grep -E "j[drx]_"))
+for ((i=0; i<${#lxk0301[*]}; i++));
+    do
+      bash jd ${lxk0301[i]} now
+done
