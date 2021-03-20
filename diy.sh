ddd25fe9f8316d9f6fb3ef12d79997b01014f6e7
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-20 18:47:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,13 +1,14 @@
 [ ! -d "/jd/diyscripts" ] && mkdir -p /jd/diyscripts
 echo "--------------------------------------------------------------------------------"
 echo "Git获取https://github.com/lmh77/customize"
-[ ! -d "/jd/diyscripts/lmh77_customize/.git" ]  \
-        && git clone https://github.com/lmh77/customize.git /jd/diyscripts/lmh77_customize \
-        && chmod +x /jd/diyscripts/lmh77_customize/*.sh \
+[ ! -d "/jd/diyscripts/lmh77_customize/.git" ]  && git clone https://github.com/lmh77/customize.git /jd/diyscripts/lmh77_customize
+
+cd /jd/diyscripts/lmh77_customize && git reset --hard && git pull \
         && cp -f /jd/diyscripts/lmh77_customize/sendinfo.sh /jd/scripts/sendinfo.sh \
         && cp -f /jd/diyscripts/lmh77_customize/sendinfo.js /jd/scripts/sendinfo.js \
-        && chmod +x /jd/scripts/sendinfo.sh
-cd /jd/diyscripts/lmh77_customize && git pull
+        && chmod +x /jd/scripts/sendinfo.sh \
+        && ln -sf cp -f /jd/diyscripts/lmh77_customize/runall.sh /usr/bin/ruanall \
+        && chmod +x /usr/local/bin/ruanall
 echo "--------------------------------------------------------------------------------"
 echo -e "开始获取自定义任务......\n" \
         && echo -e "Git获取https://github.com/i-chenzhe/qx\n" \
