46a04df74def41d04d7c9595d9c5372e80ea7128
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-03-21 22:46:59 +0800

    Update my_diy_shell.sh

--- docker1/my_diy_shell.sh
+++ docker1/my_diy_shell.sh
@@ -14,3 +14,49 @@ let bububaocookie = {
 module.exports = bububaocookie
 EOF
 ) >/scripts/bububaoCOOKIE.js
+
+# COPY from https://github.com/iouAkira/someDockerfile/tree/master/jd_scripts
+crontab -l > /scripts/befor_cronlist.sh
+
+echo "附加功能1，cookie写入文件，为jd_bot扫码获自动取cookies服务"
+if [ 0"$JD_COOKIE" = "0" ]; then
+    if [ -f "$COOKIES_CONF" ]; then
+        echo '' >$COOKIES_CONF
+        echo "未配置JD_COOKIE环境变量，$COOKIES_CONF文件已生成,请将cookies写入$COOKIES_CONF文件，格式每个Cookie一行"
+    fi
+else
+    if [ -f "$COOKIES_CONF" ]; then
+        echo "cookies.conf文件已经存在跳过,如果需要更新cookie请修改$COOKIES_CONF文件内容"
+    else
+        echo "环境变量 cookies写入$COOKIES_CONF文件,如果需要更新cookie请修改cookies.conf文件内容"
+        echo $JD_COOKIE | sed "s/\( &\|&\)/\\n/g" >$COOKIES_CONF
+    fi
+fi
+
+echo "附加功能2，拉取 i-chenzhe 仓库的代码，并增加相关任务"
+if [ -d "/i-chenzhe" ]; then
+    cd /i-chenzhe
+    git reset --hard
+    echo "git pull拉取最新代码..."
+    git -C /i-chenzhe pull --rebase
+else
+    git clone https://github.com/i-chenzhe/qx.git /i-chenzhe
+fi
+
+cd /i-chenzhe
+for scriptFile in $(ls | grep -E "jd_|z_" | tr "\n" " "); do
+    if [ -n "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $scriptFile)" ]; then
+        cp $scriptFile /scripts
+        if [ -z "$(cat /scripts/befor_cronlist.sh | grep $scriptFile)" ]; then
+            if [ $1 ];then
+                echo "skip"
+            else
+                echo "发现以前crontab里面不存在的任务，先跑为敬 $scriptFile"
+                nohup node /scripts/$scriptFile |ts >>/scripts/logs/$(echo $scriptFile | sed "s/.js/.log/g") 2>&1 &
+            fi
+        fi
+        echo "#$(sed -n "s/.*new Env('\(.*\)').*/\1/p" $scriptFile)" >>/scripts/docker/merged_list_file.sh
+        echo "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $scriptFile) spnode /scripts/$scriptFile |ts >>/scripts/logs/$(echo $scriptFile | sed "s/.js/.log/g") 2>&1 &" >>/scripts/docker/merged_list_file.sh
+    fi
+done
+crontab -l > /scripts/logs/.crontab.sh
